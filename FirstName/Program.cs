9f2e66d4a34bbbf6c6560de1529fae6c98a8f22e
Opilane TTHK <opilane@tthk.ee>
2021-09-01 12:24:54 +0300

    add readline

---
+++ FirstName/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FirstName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello, what is your first name?");
+             string Firstname = Console.ReadLine();
+             Console.WriteLine("What is your surname?");
+             string Surname = Console.ReadLine();
+             Console.WriteLine($"Hello {Firstname} {Surname}");
+
+
+        }
+    }
+}
---
+++ Howoldareyou/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Howoldareyou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Name?!");
+            string UserName = Console.ReadLine();
+            //Console.WriteLine("Hello," + UserName + "!");
+            //String interpolation
+            Console.WriteLine($"Hello, {UserName}");
+
+            Console.WriteLine("How old are you? Enter digits only;");
+
+            int Age = Int32.Parse(Console.ReadLine());
+
+            int YearOfBirth = 2021 - Age;
+            //Console.WriteLine("You were born in ");
+            Console.WriteLine($"You were born in {YearOfBirth}");
+
+
+
+        }
+    }
+}
